5fc20828a78587b5b80b231d4c6cf9eeebac3c2f
Yulei Zhu <zhuyulei12333@gmail.com>
2021-11-12 22:30:19 +1100

    feat: theme switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,62 @@
-import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
+import React from 'react'
+
+import { useTheme, ThemeProvider, createTheme } from '@mui/material/styles'
+import LightMode from '@mui/icons-material/LightMode'
+import DarkMode from '@mui/icons-material/DarkMode'
+import { Box, IconButton } from '@mui/material'
+
+const ColorModeContext = React.createContext({ toggleColorMode: () => {} })
 
 function App() {
-  const [count, setCount] = useState(0)
+  const theme = useTheme()
+  const colorMode = React.useContext(ColorModeContext)
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <Box
+      sx={{
+        display: 'flex',
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        bgcolor: 'background.default',
+        color: 'text.primary',
+        height: '100vh'
+      }}
+    >
+      <IconButton onClick={colorMode.toggleColorMode}>
+        {theme.palette.mode === 'dark' ? <DarkMode /> : <LightMode />}
+      </IconButton>
+    </Box>
   )
 }
 
-export default App
+export default function ToggleColorMode() {
+  const [mode, setMode] = React.useState<'light' | 'dark'>('dark')
+  const colorMode = React.useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode(prevMode => (prevMode === 'light' ? 'dark' : 'light'))
+      }
+    }),
+    []
+  )
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode
+        }
+      }),
+    [mode]
+  )
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  )
+}
