4630f015a7fa045c17f6b4d705ca5a6cef6a0ddb
Yulei Zhu <zhuyulei12333@gmail.com>
2022-01-14 18:57:54 +1100

    feat: home page

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import { GlobalContext } from '../../App'
 import { useTheme, ThemeProvider, createTheme } from '@mui/material/styles'
-import LightMode from '@mui/icons-material/LightMode'
-import DarkMode from '@mui/icons-material/DarkMode'
 import { Box, IconButton, Typography } from '@mui/material'
 import hi from '../../static/images/Hi.gif'
+import Contact from './src/Contact'
 
 export default function Footer() {
   const globalContext = React.useContext(GlobalContext)
@@ -16,11 +15,48 @@ export default function Footer() {
         width: '600px'
       }}
     >
-      <Typography variant='h3'>
+      <Typography variant='h4'>
         Hi there, I'm Yulei <img src={hi} height='40px'></img>
       </Typography>
       <br />
       Front-end / Full-stack developer
+      <br />
+      <br />
+      <br />
+      <Typography variant='h5'>Education</Typography>
+      <br />
+      word tropical entirely peace largest cent temperature member object castle
+      then taken sense wagon elephant flower field factory chain fix weight lamp
+      protection depthall burn than ill branch promised type snake nose
+      community property income afraid brown badly many equally pure improve
+      dinner elephant egg scale opportunity
+      <br />
+      <br />
+      <br />
+      <Typography variant='h5'>Work</Typography>
+      <br />
+      word tropical entirely peace largest cent temperature member object castle
+      then taken sense wagon elephant flower field factory chain fix weight lamp
+      protection depthall burn than ill branch promised type snake nose
+      community property income afraid brown badly many equally pure improve
+      dinner elephant egg scale opportunity
+      <br />
+      <br />
+      <br />
+      <Typography variant='h5'>About</Typography>
+      <br />
+      word tropical entirely peace largest cent temperature member object castle
+      then taken sense wagon elephant flower field factory chain fix weight lamp
+      protection depthall burn than ill branch promised type snake nose
+      community property income afraid brown badly many equally pure improve
+      dinner elephant egg scale opportunity
+      <br />
+      <br />
+      <br />
+      <Typography variant='h5'>To find me </Typography>
+      <br />
+      <Contact />
+      <br />
     </Box>
   )
 }
---
+++ src/components/Intro/src/Contact.tsx
@@ -0,0 +1,79 @@
+import React from 'react'
+import { GlobalContext } from '../../../App'
+import { useTheme, ThemeProvider, createTheme } from '@mui/material/styles'
+import { Box, IconButton, Typography, Link } from '@mui/material'
+import { LinkedIn, GitHub, Email } from '@mui/icons-material'
+import JueJin from '../../../static/images/juejin-dark.svg'
+import JueJinDark from '../../../static/images/juejin.svg'
+
+export default function Footer() {
+  const globalContext = React.useContext(GlobalContext)
+  const theme = useTheme()
+  console.log(theme.palette.mode)
+
+  return (
+    <>
+      <Link
+        href='https://github.com/Vyse12138'
+        target='_blank'
+        underline='hover'
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          color: 'inherit'
+        }}
+      >
+        <GitHub sx={{ paddingRight: '5px' }} /> @Vyse12138
+      </Link>
+
+      <br />
+
+      <Link
+        href='https://www.linkedin.com/in/yulei-zhu-902193195/'
+        target='_blank'
+        underline='hover'
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          color: 'inherit'
+        }}
+      >
+        <LinkedIn sx={{ paddingRight: '5px' }} /> @yulei
+      </Link>
+      <br />
+
+      <Link
+        href='https://juejin.cn/user/1407794523416350'
+        target='_blank'
+        underline='hover'
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          color: 'inherit'
+        }}
+      >
+        {theme.palette.mode === 'dark' ? (
+          <img src={JueJin} height='24px' />
+        ) : (
+          <img src={JueJinDark} height='24px' />
+        )}
+        <Box sx={{ paddingRight: '5px' }} /> @小诸
+      </Link>
+
+      <br />
+
+      <Link
+        href='mailto:yulei.zhu@outlook.com'
+        target='_blank'
+        underline='hover'
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          color: 'inherit'
+        }}
+      >
+        <Email sx={{ paddingRight: '5px' }} /> yulei.zhu@outlook.com
+      </Link>
+    </>
+  )
+}
